61e20b51399e5d05a3c35507a33727636f0df9e8
Cariad Eccleston <cariad@cariad.earth>
2025-06-14 15:26:55 +0000

    Improve pipeline test

--- tests/unit/stacks/test_pipeline.py
+++ tests/unit/stacks/test_pipeline.py
@@ -7,9 +7,17 @@ from iac import stacks
 @fixture
 def template(app: cdk.App) -> cdk.assertions.Template:
     stack = stacks.Pipeline(app, "Pipeline")
-
     return cdk.assertions.Template.from_stack(stack)
 
 
 def test(template: cdk.assertions.Template) -> None:
-    template.resource_count_is("AWS::CodePipeline::Pipeline", 1)
+    template.has_resource_properties(
+        "AWS::CodePipeline::Pipeline",
+        {
+            "Stages": [
+                {"Name": "Source"},
+                {"Name": "Build"},
+                {"Name": "UpdatePipeline"},
+            ],
+        },
+    )
