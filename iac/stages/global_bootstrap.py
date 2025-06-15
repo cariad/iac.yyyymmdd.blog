88db28f3fea40a4617ee36b92ba4faf7587229ff
Cariad Eccleston <cariad@cariad.earth>
2025-06-15 10:18:15 +0000

    Add Global Bootstrap stage

---
+++ iac/stages/global_bootstrap.py
@@ -0,0 +1,47 @@
+from typing import Any
+
+import aws_cdk as cdk
+from constructs import Construct
+
+from iac import stacks
+
+
+class GlobalBootstrap(cdk.Stage):
+    """
+    Global bootstrap.
+
+    Regardless of any specified environment, this stack will always be deploy to
+    us-east-1.
+
+    Args:
+        scope: Scope.
+        construct_id: Construct ID.
+        account: Amazon Web Services account ID.
+        domain_name: Domain name.
+    """
+
+    def __init__(
+        self,
+        scope: Construct,
+        construct_id: str,
+        account: str,
+        domain_name: str,
+        **kwargs: Any,
+    ) -> None:
+        global_environment = cdk.Environment(
+            account=account,
+            region="us-east-1",
+        )
+
+        super().__init__(
+            scope,
+            construct_id,
+            env=global_environment,
+            **kwargs,
+        )
+
+        stacks.Certificate(
+            self,
+            f"{construct_id}-Certificate",
+            domain_name=domain_name,
+        )
