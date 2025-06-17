2221d3697fba4685747ca8a05c1ffbdd8ba79810
Cariad Eccleston <cariad@cariad.earth>
2025-06-17 10:55:19 +0100

    Redirect from root to www subdomain (#23)

--- iac/cloudfront_functions/viewer_request.js
+++ iac/cloudfront_functions/viewer_request.js
@@ -1,5 +1,17 @@
 function handler(event) {
   var request = event.request;
+  var host = request.headers.host.value
+
+  if (!host.startsWith("www."))
+    return {
+      headers: {
+        location: {
+          value: "https://www." + host + request.uri
+        }
+      },
+      statusCode: 301,
+      statusDescription: "Moved Permanently"
+    };
 
   if (request.uri.endsWith("/")) {
     request.uri += "index.html";
